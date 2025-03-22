6833fe754bb2296050b1301b5838510a5945d67f
Elvira-dd <111658129+Elvira-dd@users.noreply.github.com>
2025-03-22 14:05:00 +0300

    rec_page_done

--- app/controllers/recommendation_controller.rb
+++ app/controllers/recommendation_controller.rb
@@ -2,5 +2,8 @@ class RecommendationController < ApplicationController
   def index
     @themes = Theme.all
     @podcasts_top = Podcast.where(id: [2, 6, 4, 7, 1, 8,10,3])
+    @popular_authors = Author.where(id: [1,3,4])
+    @popular_podcast = Podcast.find(17)
+    @feed = (Issue.where(id: [3, 4, 5, 6, 11, 8, 9]).to_a + Post.where(id: [2, 3, 4, 5]).to_a).shuffle
   end
 end
