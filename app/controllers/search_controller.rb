7b92120e71a263a49e6cda6230081076a10533f7
Elvira-dd <111658129+Elvira-dd@users.noreply.github.com>
2025-03-24 17:16:23 +0300

    seacrh_res_done

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,22 @@
 class SearchController < ApplicationController
-    def index
-      @query = params[:query]
-      if @query.present?
-        @results = Podcast.where("LOWER(name) LIKE ? OR LOWER(description) LIKE ?", "%#{@query.downcase}%", "%#{@query.downcase}%")
-      else
-        @results = []
-      end
+  def index
+    @query = params[:query].downcase
+    @type = params[:type]
+
+    case @type
+    when "posts"
+      @results = Post.where("LOWER(title) LIKE ?", "%#{@query}%")
+    when "podcasts"
+      @results = Podcast.where("LOWER(name) LIKE ?", "%#{@query}%")
+    when "issues"
+      @results = Issue.where("LOWER(name) LIKE ?", "%#{@query}%")
+    when "authors"
+      @results = Author.joins(user: :profile).where("LOWER(profiles.name) LIKE ?", "%#{@query}%")
+    else
+      @results = (Post.where("LOWER(title) LIKE ?", "%#{@query}%") +
+                  Podcast.where("LOWER(name) LIKE ?", "%#{@query}%") +
+                  Issue.where("LOWER(name) LIKE ?", "%#{@query}%") +
+                  Author.joins(user: :profile).where("LOWER(profiles.name) LIKE ?", "%#{@query}%")).uniq
     end
   end
+end
